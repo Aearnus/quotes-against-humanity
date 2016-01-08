a1fe51278802ee0353c09d994def7014f3faa1e1
T L <t>
2016-01-08 15:39:35 -0700

    timers on page

--- public/parseQuotes.rb
+++ public/parseQuotes.rb
@@ -1,9 +1,9 @@
 # encoding: UTF-8
 require "json"
 quotes = File.open("quotes.txt", "r:UTF-8", &:read).split("\n").map(&:rstrip)
-p quotes
+authors = File.open("authors.txt", "r:UTF-8", &:read).split("\n").map{|names| names.split(",")}
 quoteObject = []
 quotes.each_with_index do |quote, index|
-	quoteObject << {id: index, quote: quote}
+	quoteObject << {id: index, quote: quote, authors: authors[index]}
 end
 File.write("cards.json", JSON.generate(quoteObject));
