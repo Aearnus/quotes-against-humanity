6b5eff31fa421df987af25eb7da55893edabb2ad
T L <t>
2016-01-10 22:02:50 -0700

    new cards

--- main.rb
+++ main.rb
@@ -74,8 +74,8 @@ end
 #--------GAME OPTIONS--------
 $CARDS = File.read("public/cards.json")
 $BLACK_CARDS = File.read("public/blackCards.json")
-$NUMBER_OF_WHITE_CARDS = 378
-$NUMBER_OF_BLACK_CARDS = 48
+$NUMBER_OF_WHITE_CARDS = 383
+$NUMBER_OF_BLACK_CARDS = 62
 $CARDS_IN_A_HAND = 12
 $MAX_TIME = 40
 #--------GAME FUNCTIONS--------
